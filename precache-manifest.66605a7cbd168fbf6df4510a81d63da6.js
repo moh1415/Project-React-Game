a22f1eec11a8a875a3ecfa5de28b132ef285a18e
moh1415 <moh14151415@gmail.com>
2020-02-26 16:05:27 +0300

    Updates

---
+++ precache-manifest.66605a7cbd168fbf6df4510a81d63da6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27dc1b99b8eccf482a2994c2de875826",
+    "url": "/Project-React-Game/index.html"
+  },
+  {
+    "revision": "a2ff95f777a163b60a9d",
+    "url": "/Project-React-Game/static/css/2.da5995df.chunk.css"
+  },
+  {
+    "revision": "abed6bad9647e6281159",
+    "url": "/Project-React-Game/static/css/main.cc1b0a9e.chunk.css"
+  },
+  {
+    "revision": "a2ff95f777a163b60a9d",
+    "url": "/Project-React-Game/static/js/2.c0a0d461.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Project-React-Game/static/js/2.c0a0d461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abed6bad9647e6281159",
+    "url": "/Project-React-Game/static/js/main.5e19d13f.chunk.js"
+  },
+  {
+    "revision": "3c72758d697591143ea9",
+    "url": "/Project-React-Game/static/js/runtime-main.40ab77b8.js"
+  }
+]);
